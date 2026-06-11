48193247320d0bbff145e45164b88f73f8adc509
Lannyga <lannylovesb@gmail.com>
2026-06-11 13:01:11 -0400

    Agregar clases minimas de backend1 para integracion con grafo

---
+++ fibra-optica-voraces/src/main/java/com/fibra/backend1/graph/Grafo.java
@@ -0,0 +1,47 @@
+package com.fibra.backend1.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grafo {
+
+    private List<Nodo> nodos;
+    private List<Arista> aristas;
+
+    public Grafo() {
+        nodos = new ArrayList<>();
+        aristas = new ArrayList<>();
+    }
+
+    public void agregarNodo(Nodo nodo) {
+        nodos.add(nodo);
+    }
+
+    public void agregarArista(Arista arista) {
+        aristas.add(arista);
+    }
+
+    public List<Nodo> obtenerVecinos(Nodo nodo) {
+        List<Nodo> vecinos = new ArrayList<>();
+
+        for (Arista arista : aristas) {
+            if (arista.getOrigen().equals(nodo)) {
+                vecinos.add(arista.getDestino());
+            }
+
+            if (arista.getDestino().equals(nodo)) {
+                vecinos.add(arista.getOrigen());
+            }
+        }
+
+        return vecinos;
+    }
+
+    public List<Nodo> getNodos() {
+        return nodos;
+    }
+
+    public List<Arista> getAristas() {
+        return aristas;
+    }
+}
